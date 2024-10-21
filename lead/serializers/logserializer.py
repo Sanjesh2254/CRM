f477fd531e80363b68fa6fc65318244685bb6d3e
Afsalsyed <afsalsyed12@gmail.com>
2024-10-21 10:23:58 +0530

    Optimization !

--- lead/serializers/logserializer.py
+++ lead/serializers/logserializer.py
@@ -2,6 +2,19 @@ from rest_framework import serializers
 from ..models import Log
 
 class LogSerializer(serializers.ModelSerializer):
+    contact = serializers.SerializerMethodField()
+
     class Meta:
         model = Log
         fields = '__all__'
+
+    def get_contact(self, obj):
+        return obj.contact.name if obj.contact else None
+    
+
+
+
+
+
+
+    #contact, focus_segment, log_stage, created_by
